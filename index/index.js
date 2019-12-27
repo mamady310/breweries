9da69a1e4cbad3dff1a1666a79b258022ac54153
Mamady Sillah <supurpisces310@msn.com>
2019-12-27 14:27:26 -0500

    created a path for page Breweries by city

--- index/index.js
+++ index/index.js
@@ -2,4 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Virginia Breweries'))
+
+app.get('/city', (req, res) => res.send ('Breweries by City'))
+
 app.listen(5000, () => console.log('Example app listening on port 5000!'))
