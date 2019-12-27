39e4994c7febda8900180d3d2b846997f9c72516
Mamady Sillah <supurpisces310@msn.com>
2019-12-27 14:49:06 -0500

    changed response from shots to beer

--- db/Controller/controller.js
+++ db/Controller/controller.js
@@ -3,8 +3,10 @@ const Beer = require("../../Models/Brewery.js")
 
 module.exports = {
     drinks: (req, res) => {
-        Beer.find({}).then( (shots) => {
-            res.json(shots)
+        Beer.find({}).then( (beer) => {
+            res.json(beer)
         })
     }
-}
+},
+ 
+
--- index/index.js
+++ index/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Virginia Breweries'))
+app.get('/brewery', (req, res) => res.send(''))
 
-app.get('/city', (req, res) => res.send ('Breweries by City'))
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'))
