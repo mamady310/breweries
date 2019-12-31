65fa525dcdcfeb4508f98a131426e17acdea09a7
Mamady Sillah <supurpisces310@msn.com>
2019-12-31 14:42:43 -0500

    ran the command npm install cors

--- db/seed.js
+++ db/seed.js
@@ -1,17 +1,7 @@
 const Brewery = require('../models/Brewery.js')
 const data = require('./data.json')
 
-// const breweryData = data.map(item => {
-//     const brewery = {}
-//     brewery.name = item.name
-//     brewery.city = item.city
-//     brewery.state = item.state
-//     brewery.phone = item.phone
-//     brewery.website_url = item.website_url
 
-//     return brewery
-// })
-// console.log(breweryData)
 
 Brewery.remove({})
     .then(() => {
